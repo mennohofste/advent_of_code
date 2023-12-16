783189127db8c3be6266afaaa1744d5986713e2f
Menno <menno1337@gmail.com>
2023-12-16 02:26:16 +0200

    feat: add day 3 solution

---
+++ day_03/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+type Symbol struct {
+	IsGear bool
+	X      int
+	Y      int
+}
+
+type Number struct {
+	XStart int
+	XEnd   int
+	Y      int
+	Value  int
+}
+
+func (n Number) NextTo(symbol Symbol) bool {
+	return n.Y-symbol.Y <= 1 && symbol.Y-n.Y <= 1 && n.XStart-1 <= symbol.X && symbol.X <= n.XEnd+1
+}
+
+func getSchematic() [][]rune {
+	content, err := os.ReadFile("day_3/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var schematic [][]rune
+	for _, line := range strings.Split(string(content), "\n") {
+		schematic = append(schematic, []rune(line))
+	}
+
+	return schematic
+}
+
+func getSymbols(schematic [][]rune) []Symbol {
+	var symbols []Symbol
+	for i, line := range schematic {
+		for j, char := range line {
+			if char != '.' && !unicode.IsDigit(char) {
+				symbols = append(symbols, Symbol{char == '*', j, i})
+			}
+		}
+	}
+	return symbols
+}
+
+func getNumbers(schematic [][]rune) []Number {
+	var numbers []Number
+	for i, line := range schematic {
+		numberStart := -1
+		var numberChars []rune
+		for j, char := range line {
+			if unicode.IsDigit(char) {
+				numberChars = append(numberChars, char)
+				if numberStart == -1 {
+					numberStart = j
+				}
+			} else if numberStart != -1 {
+				numberValue, err := strconv.Atoi(string(numberChars))
+				if err != nil {
+					log.Fatal(err)
+				}
+				numbers = append(numbers, Number{numberStart, j - 1, i, numberValue})
+				numberChars = []rune{}
+				numberStart = -1
+			}
+		}
+
+		if numberStart != -1 {
+			numberValue, err := strconv.Atoi(string(numberChars))
+			if err != nil {
+				log.Fatal(err)
+			}
+			numbers = append(numbers, Number{numberStart, len(line) - 1, i, numberValue})
+		}
+	}
+	return numbers
+}
+
+func part1() int {
+	schematic := getSchematic()
+	symbols := getSymbols(schematic)
+	numbers := getNumbers(schematic)
+
+	sum := 0
+	for _, number := range numbers {
+		for _, symbol := range symbols {
+			if number.NextTo(symbol) {
+				sum += number.Value
+				break
+			}
+		}
+	}
+
+	return sum
+}
+
+func part2() int {
+	schematic := getSchematic()
+	symbols := getSymbols(schematic)
+	numbers := getNumbers(schematic)
+
+	sum := 0
+	for _, symbol := range symbols {
+		if symbol.IsGear {
+			var chosenNumbers []Number
+			for _, number := range numbers {
+				if number.NextTo(symbol) {
+					chosenNumbers = append(chosenNumbers, number)
+				}
+			}
+			if len(chosenNumbers) == 2 {
+				gearRatio := 1
+				for _, number := range chosenNumbers {
+					gearRatio *= number.Value
+				}
+				sum += gearRatio
+			}
+		}
+	}
+	return sum
+}
+
+func main() {
+	println("Part 1:", part1())
+	println("Part 2:", part2())
+}
