1f60aa55c0b0f77a2db70314b362a6ec53580056
Menno <menno1337@gmail.com>
2023-12-16 02:27:25 +0200

    feat: add day 11 solution

---
+++ day_11/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+type Universe []string
+
+func getUniverse() Universe {
+	content, err := os.ReadFile("day_11/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	content = content[:len(content)-1]
+
+	var universe Universe
+	for _, line := range strings.Split(string(content), "\n") {
+		universe = append(universe, line)
+	}
+	return universe
+}
+
+func (u Universe) String() string {
+	var s string
+	for _, line := range u {
+		s += string(line) + "\n"
+	}
+	return s
+}
+
+func (u Universe) CheckOccupation() ([]bool, []bool) {
+	occupiedRows := make([]bool, len(u))
+	occupiedCols := make([]bool, len(u[0]))
+	for i, line := range u {
+		for j, space := range line {
+			if space == '#' {
+				occupiedRows[i] = true
+				occupiedCols[j] = true
+			}
+		}
+	}
+
+	return occupiedRows, occupiedCols
+}
+
+type Galaxy struct {
+	Row int
+	Col int
+}
+
+func (u Universe) Galaxies(occupiedRows []bool, occupiedCols []bool, expansionFactor int) []Galaxy {
+	var galaxies []Galaxy
+	iOffset := 0
+	for i, row := range u {
+		jOffset := 0
+		for j, space := range row {
+			if space == '#' {
+				galaxies = append(galaxies, Galaxy{Row: i + iOffset, Col: j + jOffset})
+			}
+			if !occupiedCols[j] {
+				jOffset += expansionFactor - 1
+			}
+		}
+		if !occupiedRows[i] {
+			iOffset += expansionFactor - 1
+		}
+	}
+	return galaxies
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func part1() int {
+	universe := getUniverse()
+	occupiedRows, occupiedCols := universe.CheckOccupation()
+	galaxies := universe.Galaxies(occupiedRows, occupiedCols, 2)
+
+	totalDistance := 0
+	for i := range galaxies {
+		for j := i; j < len(galaxies); j++ {
+			totalDistance += abs(galaxies[i].Row-galaxies[j].Row) + abs(galaxies[i].Col-galaxies[j].Col)
+		}
+	}
+	return totalDistance
+}
+
+func part2() int {
+	universe := getUniverse()
+	occupiedRows, occupiedCols := universe.CheckOccupation()
+	galaxies := universe.Galaxies(occupiedRows, occupiedCols, 1000000)
+
+	totalDistance := 0
+	for i := range galaxies {
+		for j := i; j < len(galaxies); j++ {
+			totalDistance += abs(galaxies[i].Row-galaxies[j].Row) + abs(galaxies[i].Col-galaxies[j].Col)
+		}
+	}
+	return totalDistance
+}
+
+func main() {
+	println("Part 1:", part1())
+	println("Part 2:", part2())
+}
